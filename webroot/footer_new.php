a87338df2dc066bdbdb938936971fc2bc1cd9de4
Richard Gilbert <gilbert67@gmail.com>
2020-06-19 18:23:42 -0700

    remainder.

---
+++ webroot/footer_new.php
@@ -0,0 +1,11 @@
+<!-- Load React. -->
+<!-- Note: when deploying, replace "development.js" with "production.min.js". -->
+<script src="https://unpkg.com/react@16/umd/react.development.js"></script>
+<script src="https://unpkg.com/react-dom@16/umd/react-dom.development.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/babel-standalone/6.21.1/babel.min.js"></script>
+
+<!-- Load our React component. -->
+<script type="text/babel" src="/js/like_button.js"></script>
+
+</body>
+</html>
---
+++ webroot/header_new.php
@@ -0,0 +1,7 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+
+<html>
+<head>
+	<title>Rainier Atlantic</title>
+	<link rel="stylesheet" href="css/styles.css" type="text/css">
+</head>
